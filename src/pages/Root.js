544577afe5a3d17ed457661160c49b6535281d99
Khaibar Saadat <khybersaadat93@gmail.com>
2023-05-22 22:07:40 +0200

    Adding Automatic Logout but not complete, completion on the next commit

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,15 +1,27 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
-  const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>...loading</p>}
+        {/* {navigation.state === "loading" && <p>...loading</p>} */}
         <Outlet />
       </main>
     </>
